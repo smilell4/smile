9361e37bc1f6d92665fdee1a5aa0dee5db3c823f
smile <2419942029@qq.com>
2022-02-13 22:01:15 +0800

    测试修改-hotfix

--- src/com/smile/test/Test.java
+++ src/com/smile/test/Test.java
@@ -3,5 +3,6 @@ package com.smile.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hi smile");
+        System.out.println("hi smile hello");
     }
 }
